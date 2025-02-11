a0cc4f3e9422339288ed896be59ceabf3b0f9c73
Jonah Glunt <jjg56@psu.edu>
2025-02-11 13:57:57 -0500

    too buggy; Gurobi often returns infeasible MIQP (when in fact it is feasible)

---
+++ dev/testing/Testing_point_projection.m
@@ -0,0 +1,72 @@
+%%
+close all, clc, clear all
+
+Gc = eye(2);
+Gb = [2;0];
+c = [0;0];
+Z = hybZono(Gc, Gb, c, [], [], []);
+y = randn(2,1);
+
+figure
+hold on, grid on, grid minor
+plot(Z, 'b', .5)
+plot(y(1), y(2), 'g.', 'MarkerSize',18)
+
+axis([-3 3 -3 3])
+axis equal
+
+result = point_projection(Z,y);
+x = result.x(1:Z.n);
+feas_factors = result.x(Z.n+1:end);
+
+plot(x(1), x(2), 'r.', 'MarkerSize', 18)
+
+
+%%
+clear all, close all, clc
+
+% rng(3)
+M = randn(2,5);
+CZ1 = conZono([randn(2,2) [0;0]], [1;-1], randn(1,3), 0);
+CZ2 = conZono([randn(2,2) [0;0]], [10;0], randn(1,3), 0);
+% CZ3 = conZono(M, [7;-8], [randn(1,3) 0 0], 0);
+% Z = hybZono(CZ1);
+Z = union(CZ1, CZ2);
+% Z = union(CZ1, union(CZ2, CZ3));
+% Z = sharpHybZono(Z);
+y = randn(2,1);
+
+figure
+hold on, grid on, grid minor
+plot(Z, 'b', .5)
+plot(y(1), y(2), 'g.', 'MarkerSize',18)
+axis equal
+
+result = point_projection(Z,y);
+x = result.x(1:Z.n);
+feas_factors = result.x(Z.n+1:end);
+
+plot(x(1), x(2), 'r.', 'MarkerSize', 18)
+
+%%
+close all, clc, clear all
+
+Gc = eye(2);
+Gb = 2*eye(2);
+c = [0;0];
+Z = hybZono(Gc, Gb, c, [], [], []);
+y = randn(2,1);
+
+figure
+hold on, grid on, grid minor
+plot(Z, 'b', .5)
+plot(y(1), y(2), 'g.', 'MarkerSize',18)
+
+axis([-3 3 -3 3])
+axis equal
+
+result = point_projection(Z,y);
+x = result.x(1:Z.n);
+feas_factors = result.x(Z.n+1:end);
+
+plot(x(1), x(2), 'r.', 'MarkerSize', 18)
--- globalFunctions/point_projection.m
+++ globalFunctions/point_projection.m
@@ -15,29 +15,29 @@ function result = point_projection(Z, y)
     A(1:n, 1:n) = -eye(n);
     A(1:n, (n+1):(n+nGc)) = Z.Gc;
     A(1:n, (n+nGc+1):(n+nGc+nGb)) = 2*Z.Gb;
-    b(1:n) = -Z.Ab*ones(nGb,1);
+    b(1:n) = -Z.Gb*ones(nGb,1);
 
     A((n+1):end, (n+1):(n+nGc)) = Z.Ac;
     A((n+1):end, (n+nGc+1):(n+nGc+nGb)) = 2*Z.Ab;
     b((n+1):end) = Z.b - Z.Ab*ones(nGb,1);
 
-    z_lb = zeros(obj.n,1);
-    z_ub = zeros(obj.n,1);
-    basisVectors = eye(obj.n);
-    for i = 1:obj.n
-        [s,~] = supportFunc(obj,-basisVectors(:,i));
+    z_lb = zeros(n,1);
+    z_ub = zeros(n,1);
+    basisVectors = eye(n);
+    for i = 1:n
+        [s,~] = supportFunc(Z,-basisVectors(:,i));
         z_lb(i) = -s;
-        [s,~] = supportFunc(obj,basisVectors(:,i));
+        [s,~] = supportFunc(Z,basisVectors(:,i));
         z_ub(i) = s;
     end
 
     lb = [z_lb; -ones(nGc,1); zeros(nGb, 1)];
     ub = [z_ub; ones(nGc+nGb,1)];
 
-    vType(1:Z.nGc) = 'C';
-    vType(Z.nGc+1:(Z.nGc+Z.nGb)) = 'B';
+    vType(1:(n+nGc)) = 'C';
+    vType((n+nGc+1):(n+nGc+nGb)) = 'B';
     
-    model.A = A;
+    model.A = sparse(A);
     model.obj = -2*y'*Q*R;
     model.sense = '=';
     model.rhs = b;
@@ -46,7 +46,12 @@ function result = point_projection(Z, y)
     model.vtype = vType;
     model.modelsense = 'min';
     model.objcon = y'*Q*y;
-    model.Q = R'*Q*R;
+    model.Q = sparse(R'*Q*R);
 
+    params.Threads = 1;
+    params.outputFlag = 0;
+    params.PoolSearchMode = 2;
+    params.PoolSolutions = 2e9;
+    params.MIPFocus = 1;
     result = gurobi(model, params);
 end
