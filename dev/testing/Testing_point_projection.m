f787a08d5461f4985cf0ab0b77090bbc8bd84d67
Jonah Glunt <jjg56@psu.edu>
2025-02-13 10:26:18 -0500

    Found typos, now functions work as intended for hybZonos. Implemented also for conZonos/Zonos, using Gurobi again as QP solver. Seems to work well on trivial examples; not rigorously tested on large sets

--- dev/testing/Testing_point_projection.m
+++ dev/testing/Testing_point_projection.m
@@ -1,4 +1,4 @@
-%%
+%% HybZono
 close all, clc, clear all
 
 Gc = eye(2);
@@ -15,40 +15,35 @@ plot(y(1), y(2), 'g.', 'MarkerSize',18)
 axis([-3 3 -3 3])
 axis equal
 
-result = point_projection(Z,y);
-x = result.x(1:Z.n);
-feas_factors = result.x(Z.n+1:end);
+[x, xi, result] = point_projection(Z,y);
 
 plot(x(1), x(2), 'r.', 'MarkerSize', 18)
 
 
-%%
+%% HybZono
 clear all, close all, clc
 
 % rng(3)
 M = randn(2,5);
 CZ1 = conZono([randn(2,2) [0;0]], [1;-1], randn(1,3), 0);
 CZ2 = conZono([randn(2,2) [0;0]], [10;0], randn(1,3), 0);
-% CZ3 = conZono(M, [7;-8], [randn(1,3) 0 0], 0);
+CZ3 = conZono(M, [7;-8], [randn(1,3) 0 0], 0);
 % Z = hybZono(CZ1);
-Z = union(CZ1, CZ2);
-% Z = union(CZ1, union(CZ2, CZ3));
+% Z = union(CZ1, CZ2);
+Z = union(CZ1, union(CZ2, CZ3));
 % Z = sharpHybZono(Z);
 y = randn(2,1);
+
+[x, xi, result] = point_projection(Z,y);
 
 figure
 hold on, grid on, grid minor
 plot(Z, 'b', .5)
 plot(y(1), y(2), 'g.', 'MarkerSize',18)
+plot(x(1), x(2), 'r.', 'MarkerSize', 18)
 axis equal
 
-result = point_projection(Z,y);
-x = result.x(1:Z.n);
-feas_factors = result.x((Z.n+1):end);
-
-plot(x(1), x(2), 'r.', 'MarkerSize', 18)
-
-%%
+%% HybZono
 close all, clc, clear all
 
 Gc = eye(2);
@@ -57,16 +52,38 @@ c = [randn(2,1)];
 Z = hybZono(Gc, Gb, c, [], [], []);
 y = randn(2,1);
 
+[x, xi, result] = point_projection(Z,y);
+
 figure
 hold on, grid on, grid minor
 plot(Z, 'b', .5)
 plot(y(1), y(2), 'g.', 'MarkerSize',18)
+plot(x(1), x(2), 'r.', 'MarkerSize', 18)
 
-% axis([-3 3 -3 3])
-axis equal
+%% ConZono
+close all, clc, clear all
 
-result = point_projection(Z,y);
-x = result.x(1:Z.n);
-feas_factors = result.x(Z.n+1:end);
+Z = conZono(randn(2,6), randn(2,1), rand(2,6), rand(2,1));
+y = 4*randn(2,1);
 
+[x, xi, result] = point_projection(Z,y);
+
+figure
+hold on, grid on, grid minor
+plot(Z, 'b', .5)
+plot(y(1), y(2), 'g.', 'MarkerSize', 18)
+plot(x(1), x(2), 'r.', 'MarkerSize', 18)
+
+%% Zono
+close all, clc, clear all
+
+Z = zono(randn(2,6), randn(2,1));
+y = 4*randn(2,1);
+
+x = point_projection(Z,y);
+
+figure
+hold on, grid on, grid minor
+plot(Z, 'b', .5)
+plot(y(1), y(2), 'g.', 'MarkerSize', 18)
 plot(x(1), x(2), 'r.', 'MarkerSize', 18)
--- globalFunctions/point_projection.m
+++ globalFunctions/point_projection.m
@@ -1,52 +1,29 @@
-function result = point_projection(Z, y)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-    
+%   Returns the point within zonotopic set Z closest (2-norm) to the point y.
+%   Utilizes Gurobi to solve MIQPs/QPs. 
+%   
+%   Syntax: 
+%       [x, xi, result] = point_projection(Z, y)
+%   Inputs:
+%       Z   - hybZono, conZono, or zono object
+%       y   - vector of dimension [Z.n 1]
+%   Outputs:
+%       x   - point in Z closest to y
+%       xi  - factors (xi_c, xi_b) that generate x in Z
+%       result - output from GUROBI optimization (for debugging/analysis)
+function [x, xi, result] = point_projection(Z, y)    
     if size(y,1)~=Z.n || size(y,2)~=1
         error('Incompatible dimensions; y must be a column vector whose height is the dimensionality of Z.')
     end
     switch class(Z)
         case 'hybZono'
-            result = point_projection_hybZono_yalmip(Z,y);
-        case 'conZono'
-            result = point_projection_conZono(Z,y);
-        case 'zono'
-            result = point_projection_zono(Z,y);
-
+            [x, xi, result] = point_projection_hybZono(Z,y);
+        case {'conZono', 'zono'}
+            [x, xi, result] = point_projection_zono(Z,y);
     end
 end
 
-function result = point_projection_hybZono_yalmip(Z,y)
-    n = Z.n; nGc = Z.nGc; nGb = Z.nGb;
-    nX = n + nGc + nGb;
-
-    x = sdpvar(n,1);
-    xi_c = sdpvar(nGc,1);
-    xi_b = binvar(nGb,1);
-    opts = sdpsettings;
-    opts.solver = 'gurobi';
-    opts.gurobi.MIPFocus = 2;
-    opts.gurobi.OutputFlag = 0;
-    opts.gurobi.Threads = 8;
-    opts.gurobi.OptimalityTol = 1e-8;
-    J = norm(x-y,2) + 1e-4*norm([xi_c; xi_b], 2);
-    cons = [ norm(xi_c, 'inf') <= 1 ,
-             x == Z.Gc*xi_c + Z.Gb*(2*xi_b - ones(nGb,1)) + Z.c,
-             Z.b == Z.Ac*xi_c + Z.Ab*(2*(xi_b - ones(nGb,1)))];
-    result = optimize(cons,J,opts);
-    result.x = [value(x); value(xi_c); 2*value(xi_b) - 1];
-
-end
-
-
-
-
-
-
-
-function result = point_projection_hybZono(Z, y)
-    optSolver = solverOptions;
-    opSolver.milpSolver = 'gurobi';
+% implementation for hybZono
+function [x, xi, result] = point_projection_hybZono(Z, y)
     n = Z.n; nGc = Z.nGc; nGb = Z.nGb;
     nX = n + nGc + nGb;
 
@@ -58,7 +35,7 @@ function result = point_projection_hybZono(Z, y)
     A(1:n, 1:n) = -eye(n);
     A(1:n, (n+1):(n+nGc)) = Z.Gc;
     A(1:n, (n+nGc+1):(n+nGc+nGb)) = 2*Z.Gb;
-    b(1:n) = Z.Gb*ones(nGb,1) + Z.c;
+    b(1:n) = Z.Gb*ones(nGb,1) - Z.c;
 
     A((n+1):end, (n+1):(n+nGc)) = Z.Ac;
     A((n+1):end, (n+nGc+1):(n+nGc+nGb)) = 2*Z.Ab;
@@ -91,11 +68,91 @@ function result = point_projection_hybZono(Z, y)
     model.objcon = y'*Q*y;
     model.Q = sparse(R'*Q*R);
 
+    % GUROBI settings
     params.Threads = 8;
     params.outputFlag = 0;
-    params.PoolSearchMode = 2;
+    params.PoolSearchMode = 1;
     params.PoolSolutions = 2e9;
     params.MIPFocus = 1;
     result = gurobi(model, params);
+
+    x = result.x(1:n);
+    xi = result.x(n+1:end);
+    xi((nGc+1):end) = 2*xi(nGc+1:end)-1;
 end
 
+% used in prototyping
+function [x,xi,result] = point_projection_hybZono_yalmip(Z,y)
+    n = Z.n; nGc = Z.nGc; nGb = Z.nGb;
+
+    x = sdpvar(n,1);
+    xi_c = sdpvar(nGc,1);
+    xi_b = binvar(nGb,1);
+    opts = sdpsettings;
+    opts.solver = 'gurobi';
+    opts.gurobi.MIPFocus = 0;
+    opts.gurobi.OutputFlag = 0;
+    opts.gurobi.Threads = 8;
+    opts.gurobi.OptimalityTol = 1e-8;
+    opts.verbose = 0;
+    J = norm(x-y,2) + 1e-4*norm([xi_c; xi_b], 2);
+    cons = [ norm(xi_c, 'inf') <= 1 ,
+             x == Z.Gc*xi_c + Z.Gb*(2*xi_b - ones(nGb,1)) + Z.c,
+             Z.b == Z.Ac*xi_c + Z.Ab*(2*xi_b - ones(nGb,1))];
+    result = optimize(cons,J,opts);
+    x = value(x);
+    xi = [value(xi_c); 2*value(xi_b)-1];
+end
+
+function [x, xi, result] = point_projection_zono(Z,y)
+    n = Z.n; nG = Z.nG;
+    nX = n + nG;
+
+    R = eye(n,nX);
+    Q = eye(n);
+    
+        A = zeros(n + Z.nC, nX);
+        b = nan(n + Z.nC, 1);
+        A(1:n, 1:n) = -eye(n);
+        A(1:n, (n+1):end) = Z.G;
+        b(1:n) = -Z.c;
+    if isa(Z, 'conZono')
+        A((n+1):end, (n+1):(end)) = Z.A;
+        b((n+1):end) = Z.b;
+    end
+        
+    z_lb = zeros(n,1);
+    z_ub = zeros(n,1);
+    basisVectors = eye(n);
+    for i = 1:n
+        [s,~] = supportFunc(Z,-basisVectors(:,i));
+        z_lb(i) = -s;
+        [s,~] = supportFunc(Z,basisVectors(:,i));
+        z_ub(i) = s;
+    end
+
+    lb = [z_lb; -ones(nG,1)];
+    ub = [z_ub; ones(nG,1)];
+
+    vType(1:(n+nG)) = 'C';
+    model.A = sparse(A);
+    model.obj = -2*y'*Q*R;
+    model.sense = '=';
+    model.rhs = b;
+    model.lb = lb;
+    model.ub = ub;    
+    model.vtype = vType;
+    model.modelsense = 'min';
+    model.objcon = y'*Q*y;
+    model.Q = sparse(R'*Q*R);
+
+    params.Threads = 8;
+    params.outputFlag = 0;
+    params.PoolSearchMode = 1;
+    params.PoolSolutions = 2e9;
+    params.MIPFocus = 0;
+    result = gurobi(model, params);
+
+    x = result.x(1:n);
+    xi = result.x(n+1:end);
+end
