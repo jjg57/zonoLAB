b640f611d7a65b133884fc016636bb09b79735c6
Jonah Glunt <jjg57@psu.edu>
2025-10-15 12:53:00 -0400

    fixed typo in hyb2leafarray

--- @hybZono/hybZono.m
+++ @hybZono/hybZono.m
@@ -143,7 +143,7 @@ classdef hybZono < abstractZono
          % Export each leaf as a cell array of conZonos 
         function out = hyb2leafArray(obj)
             leaves = obj.getLeaves();
-            for leaf = 1: length(leaves)
+            for leaf = 1: width(leaves)
                 out{leaf,1} = conZono(obj.Gc,obj.c+obj.Gb*leaves(:,leaf),obj.Ac,obj.b-obj.Ab*leaves(:,leaf));
             end
         end
